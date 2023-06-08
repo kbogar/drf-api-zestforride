a9aebdf4a6bfdeedd71370848318ffe66ec598d8
kbogar <krisztianbogar@gmail.com>
2023-06-08 10:28:00 +0000

    Create EventSerializer, views and connect urls

--- events/views.py
+++ events/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from zestforride_api.permissions import IsOwnerOrReadOnly
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    """
+    List events or create an event if logged in
+    The perform_create method associates the event with the logged in user.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve an event and edit or delete it if you own it.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Event.objects.all()
--- zestforride_api/urls.py
+++ zestforride_api/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('likes.urls')),
     path('', include('followers.urls')),
     path('', include('contact.urls')),
+    path('', include('events.urls')),
 ]
