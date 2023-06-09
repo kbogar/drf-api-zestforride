3cd2c07ce67b0772b477b1b5f33c75e1702c1e76
kbogar <krisztianbogar@gmail.com>
2023-06-09 11:32:26 +0000

    Create Interested app, model and migrate

---
+++ interested/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
+
+
+class Interested(models.Model):
+    """
+    Model for Interested, related to Event.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='interested')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'event']
+
+    def __str__(self):
+        return f'{self.owner} {self.event}'
--- zestforride_api/settings.py
+++ zestforride_api/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'followers',
     'contact',
     'events',
+    'interested',
 ]
 
 MIDDLEWARE = [
